7ccfa2e88987b9f1ac54008e1caace921ad7c016
Kirill Shapoval <aeskelll27@gmail.com>
2023-05-22 08:55:02 +0300

    working version of RPS game score box doesn't work properly

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let playerScore = 0;
 let computerScore = 0;
-
-//choice array
+let computerSelection;
+const pScore = document.querySelector(".pScore");
+const cScore = document.querySelector(".cScore");
+const winOrLost = document.querySelector(".winOrLost");
 const myArray = ["rock", "paper", "scissors"];
 
 //this function returns string
@@ -12,38 +14,79 @@ function getComputerChoice() {
 
 //this function play one round
 function playRound(playerselection, computerSelection) {
-  computerSelection = getComputerChoice();
-  playerselection = prompt("Make your choice").toLowerCase();
   console.log(computerSelection);
   if (
     (playerselection === "paper" && computerSelection === "paper") ||
     (playerselection === "scissors" && computerSelection === "scissors") ||
     (playerselection === "rock" && computerSelection === "rock")
   ) {
-    alert("It's a tie!");
+    console.log("It's a tie!");
   } else if (
     (playerselection === "paper" && computerSelection === "rock") ||
     (playerselection === "rock" && computerSelection === "scissors") ||
     (playerselection === "scissors" && computerSelection === "paper")
   ) {
-    alert("You win!");
+    console.log("You win!");
     playerScore++;
   } else {
-    alert("You lost!");
+    console.log("You lost!");
     computerScore++;
   }
 }
 
-function game() {
-  while (playerScore != 5 && computerScore != 5) {
-    playRound();
-    console.log(`Score: player ${playerScore}, computer ${computerScore}`);
-  }
-  if (playerScore === 5) {
-    alert("You win this game!!!");
-  } else if (computerScore === 5) {
-    alert("You lose this game");
-  }
+//.startGame button resets a score and starts new game
+const playButton = document.querySelector(".startGame");
+playButton.addEventListener("click", () => {
   playerScore = 0;
   computerScore = 0;
+  pScore.textContent = playerScore;
+  cScore.textContent = computerScore;
+  rock.disabled = false;
+  paper.disabled = false;
+  scissors.disabled = false;
+  winOrLost.textContent = "";
+  document.querySelector(".scoreBox").style.display = "block";
+});
+
+//update fresh score and declare a winner
+function applyNewScore() {
+  pScore.textContent = playerScore;
+  cScore.textContent = computerScore;
+  if (playerScore == 5) {
+    winOrLost.textContent = "Congratulations! You win the game!";
+    winOrLost.style.display = "block";
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  } else if (computerScore == 5) {
+    winOrLost.textContent = "You lost the game, try again";
+    winOrLost.style.display = "block";
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  }
 }
+
+const rock = document.querySelector(".rock");
+rock.addEventListener("click", () => {
+  getComputerChoice();
+  computerSelection = getComputerChoice();
+  playRound("rock", computerSelection);
+  applyNewScore();
+});
+
+const paper = document.querySelector(".paper");
+paper.addEventListener("click", () => {
+  getComputerChoice();
+  computerSelection = getComputerChoice();
+  playRound("paper", computerSelection);
+  applyNewScore();
+});
+
+const scissors = document.querySelector(".scissors");
+scissors.addEventListener("click", () => {
+  getComputerChoice();
+  computerSelection = getComputerChoice();
+  playRound("scissors", computerSelection);
+  applyNewScore();
+});
